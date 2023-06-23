c9ab2b76c7c0f7e465ba5eef88c1f470d99a74db
Kelvem01 <kelvemdasilva16@gmail.com>
2023-06-23 10:30:51 -0300

    decoradores

--- program_pinc.py
+++ program_pinc.py
@@ -3,6 +3,6 @@ from funcoes import juros_simples
 
 capital = float(input('digite o capital:'))
 taxa = float(input('digite o tempo: '))
-tempo = float(input('digite o tempo: '))
-                    
+tempo = float(input('digite o tempo: ' ))
+
 juros_simples(capital, taxa , tempo )
